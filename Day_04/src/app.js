e3c7194c72c94d7ab7a0d4b5619a77c806337c8e
Anuj Dhauskar <anujdhauskar64@gmail.com>
2026-02-03 14:02:02 +0530

    day4

--- Day_04/src/app.js
+++ Day_04/src/app.js
@@ -3,3 +3,9 @@ server ko create karna
 server ko config karna
 
 */ 
+
+const express = require('express');
+
+const app = express();
+
+module.exports = app;
--- Day_04/src/server.js
+++ Day_04/src/server.js
@@ -1,3 +1,20 @@
 /*
 server ko start karna
+
  */
+
+const app = require('./app')
+const mongoose = require("mongoose");
+
+function connectToDb(){
+    mongoose.connect("")
+    .then(()=>{
+        console.log("connected to db")
+    })
+}
+connectToDb();
+
+
+app.listen(3000,()=>{
+    console.log('server is running on port 3000')
+})
