05522212bf736e582982bbc1c5f27f7be0aaa654
Anuj Dhauskar <anujdhauskar64@gmail.com>
2026-02-28 16:39:33 +0530

    FrontEnd and Backend Integration using axios , created login and Register form

---
+++ Project-1/Backend/src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cookieparser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cookieparser());
+app.use(cors(
+
+    {
+        credentials:true,
+        origin:"http://localhost:5173"
+    }
+))
+
+/*require Routes*/
+const authRouter = require("./routes/auth.routes")
+const postRouter = require("./routes/post.routes")
+const userRouter = require("./routes/user.routes")
+
+
+/*using routes */
+app.use("/api/auth",authRouter)
+app.use("/api/posts",postRouter)
+app.use("/api/users",userRouter)
+
+module.exports = app;
---
+++ Project-1/Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import "../style/form.scss"
+import { Link } from 'react-router'
+import { useState } from 'react'
+
+const Login = () => {
+
+  const [username, setusername] = useState("")
+  const [password, setpassword] = useState("")
+
+  async function handleSubmit(e){
+    e.preventDefault()
+
+    axios.post("http://localhost:3000/api/auth/login",{
+        username,
+        password
+    },{
+        withCredentials:true
+    })
+
+    .then(res=>{
+        console.log(res.data)
+    })
+  }
+
+  return (
+    <main>
+      <div className="form-container">
+
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+        <input onInput={(e)=>{setusername(e.target.value)}} type="text" name='username' placeholder='Enter Username'/>
+        <input onInput={(e)=>{setpassword(e.target.value)}} type="text" name='password' placeholder='Enter Password' />
+        <button type='submit'>Login</button>
+        </form>
+
+        <p>Don't have an account? <Link className='toggleAuthForm' to="/register">Register</Link></p>
+      </div>
+    </main>
+  )
+}
+
+export default Login
